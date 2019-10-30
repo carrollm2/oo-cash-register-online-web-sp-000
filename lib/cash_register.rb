a9c026be82fedb2878f9a5615bf60cb0fa530f7c
Michael Carroll <carrollm2@gmail.com>
2019-10-30 22:10:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -32,7 +32,6 @@ class CashRegister
     all_transactions.each do |transaction_event|
       counter = 0
       transaction_event.each do |item_name, cost_and_quantity|
-        print cost_and_quantity
         while counter < cost_and_quantity[1]
           items.push(item_name)
           counter += 1
